cdff2e34e11d5ef9094b7615d57c23a1af799377
Patryk Pastuszka <nov11157@gmail.com>
2025-05-18 19:36:42 +0200

    Awaiters for BluetoothLE devices discovery and configuration

---
+++ Utility/ValueTaskExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace IRIS.Utility
+{
+    public static class ValueTaskExtensions
+    {
+        public static async void Forget(this ValueTask valueTask)
+        {
+            await valueTask;
+        }
+    }
+}
